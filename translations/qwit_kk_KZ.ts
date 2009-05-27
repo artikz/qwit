6bce542d0759f173f273431331c3b8186616665c
artikz <artem.iglikov@gmail.com>
2009-05-27 13:00:01 +0000

    Added Kazakh translation - thanks to Aslan Kokanbekov
    git-svn-id: http://qwit.googlecode.com/svn/trunk@128 067a171b-6953-0410-b330-3d32600bf046

---
+++ translations/qwit_kk_KZ.ts
@@ -0,0 +1,377 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="kk_KZ">
+<context>
+    <name>LogsDialog</name>
+    <message>
+        <location filename="../src/LogsDialog.ui" line="13"/>
+        <source>Logs</source>
+        <translation>Журналдар</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="218"/>
+        <source>&amp;Show / Hide</source>
+        <translation>&amp;Көрсету / Жасыру</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="220"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Шығу</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="741"/>
+        <source>Qwit updates</source>
+        <translation>Qwit жаңартулары</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="13"/>
+        <source>Qwit</source>
+        <translation>Qwit</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="39"/>
+        <source>What are you doing?</source>
+        <translation>Қазір сіз не істеп жатырсыз?</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="205"/>
+        <source>Left characters count - please, do not exceed this number</source>
+        <translation>Қалған қаріптердің саны, одан артық жазбауыңызды өтінеміз</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="214"/>
+        <source>140</source>
+        <translation>140</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="236"/>
+        <source>Home</source>
+        <translation>Негізгі</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="239"/>
+        <source>Your and your friends&apos; timeline</source>
+        <translation>Сіздің және достарыңыздың мәлімдемелері</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="244"/>
+        <source>Public</source>
+        <translation>Бәрі</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="247"/>
+        <source>Public timeline</source>
+        <translation>Барлығының мәлімдемелері</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="252"/>
+        <source>Replies</source>
+        <translation>Жауаптар</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="255"/>
+        <source>Replies to you</source>
+        <translation>Сізге жауаптар</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="260"/>
+        <source>Custom</source>
+        <translation>Бөлек</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="263"/>
+        <source>Custom user timeline</source>
+        <translation>Бөлек алынған қолданушының мәлімдемелері</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="273"/>
+        <source>Username:</source>
+        <translation>Пайдаланушының аты:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="369"/>
+        <source>Refresh</source>
+        <translation>Жаңарту</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="379"/>
+        <source>Ctrl+R</source>
+        <translation>Ctrl+R</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="398"/>
+        <source>Options</source>
+        <translation>Икемдеулер</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="441"/>
+        <source>Quit</source>
+        <translation>Шығу</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="288"/>
+        <source>Inbox</source>
+        <translation>Кіріс</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="291"/>
+        <source>Direct messages inbox</source>
+        <translation>Кіріс мәлімдемелері</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="296"/>
+        <source>Outbox</source>
+        <translation>Шығыс</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="299"/>
+        <source>Direct messages outbox</source>
+        <translation>Шығыс мәлімдемелері</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="304"/>
+        <source>Search</source>
+        <translation>Іздеу</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="317"/>
+        <source>Search:</source>
+        <translation>Іздеу:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="307"/>
+        <source>Search for messages</source>
+        <translation>Мәлімдемелерді іздеу</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsDialog</name>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="13"/>
+        <source>Options</source>
+        <translation>Икемдеулер</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="25"/>
+        <source>General</source>
+        <translation>Жалпы</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="66"/>
+        <source>Update interval:</source>
+        <translation>Жаңартулар аралығы:</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="79"/>
+        <source>Number of messages per page:</source>
+        <translation>Беттегі мәлімдемелер саны:</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="184"/>
+        <source>Connection</source>
+        <translation>Қосылыс</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="192"/>
+        <source>use proxy</source>
+        <translation>Проксиді қолдану</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="199"/>
+        <source>Proxy address:</source>
+        <translation>Прокси-сервердің мекені:</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="212"/>
+        <source>Proxy port:</source>
+        <translation>Прокси-сервердің порты:</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="225"/>
+        <source>Proxy username:</source>
+        <translation>Пайдаланушының аты:</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="238"/>
+        <source>Proxy password:</source>
+        <translation>Құпиясөз:</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="255"/>
+        <source>save proxy password</source>
+        <translation>Прокси-сервердің құпиясөзін сақтау</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="278"/>
+        <source>Authentication</source>
+        <translation>Аутентификация</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="286"/>
+        <source>Username or email:</source>
+        <translation>Пайдаланушының аты немесе email:</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="299"/>
+        <source>Password:</source>
+        <translation>Құпиясөз:</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="316"/>
+        <source>save password</source>
+        <translation>Құпиясөзді сақтау</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="361"/>
+        <source>Save options</source>
+        <translation>Икемдеулерді сақтау</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="368"/>
+        <source>Reset options</source>
+        <translation>Икемдеулерді қайтару</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="46"/>
+        <source>Service base URL:</source>
+        <translation>Қызметтің негізгі мекені:</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="56"/>
+        <source>Service API URL:</source>
+        <translation>Қызметтің API-нің мекені:</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="148"/>
+        <source>Notify about updates</source>
+        <translation>Жаңартулар туралы мәлімдеу</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="36"/>
+        <source>Greeting message:</source>
+        <translation>Сәлемдесу:</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="92"/>
+        <source>Number of messages per tray:</source>
+        <translation>Хабарламадағы мәлімдемелер саны:</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="138"/>
+        <source>Username under avatar</source>
+        <translation>Пайдаланушының атын бейненің астына</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="115"/>
+        <source>Display retweet tag after text instead of in front</source>
+        <translation>Қайта-твиттеу тэгін мәтіннің алдында емес, соңында көрсету</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="105"/>
+        <source>Retweeting tag:</source>
+        <translation>Қайта-твиттеу тэгі:</translation>
+    </message>
+    <message>
+        <location filename="../src/OptionsDialog.ui" line="158"/>
+        <source>Place controls vertically</source>
+        <translation>Батырмаларды тігінен қою</translation>
+    </message>
+</context>
+<context>
+    <name>Twitter</name>
+    <message>
+        <location filename="../src/Twitter.cpp" line="140"/>
+        <source>Error sending status: %1</source>
+        <translation>Ахуалды жіберу қатесі: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/Twitter.cpp" line="143"/>
+        <source>Status sent: %1</source>
+        <translation>Ахуал жіберілді: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/Twitter.cpp" line="149"/>
+        <source>Error while updating timeline: %1</source>
+        <translation>Мәлімдемелерді жаңарту қатесі: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/Twitter.cpp" line="153"/>
+        <source>Timeline updated: %1</source>
+        <translation>Мәлімдемелер жаңартылды: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/Twitter.cpp" line="134"/>
+        <source>Updating timeline: %1</source>
+        <translation>Мәлімдемелерді жаңарту: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/Twitter.cpp" line="95"/>
+        <source>Sending status: %1</source>
+        <translation>Ахуалды жіберу: %1</translation>
+    </message>
+</context>
+<context>
+    <name>TwitterWidget</name>
+    <message numerus="yes">
+        <location filename="../src/TwitterWidget.cpp" line="267"/>
+        <source>about %n second(s) ago</source>
+        <translation>
+            <numerusform>шамамен %n секунд бұрын</numerusform>
+            <numerusform>шамамен %n секунд бұрын</numerusform>
+            <numerusform>шамамен %n секунд бұрын</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/TwitterWidget.cpp" line="269"/>
+        <source>about %n minute(s) ago</source>
+        <translation>
+            <numerusform>шамамен %n минут бұрын</numerusform>
+            <numerusform>шамамен %n минут бұрын</numerusform>
+            <numerusform>шамамен %n минут бұрын</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/TwitterWidget.cpp" line="271"/>
+        <source>about %n hour(s) ago</source>
+        <translation>
+            <numerusform>шамамен %n сағат бұрын</numerusform>
+            <numerusform>шамамен %n сағат бұрын</numerusform>
+            <numerusform>шамамен %n сағат бұрын</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/TwitterWidget.cpp" line="273"/>
+        <source>about %n day(s) ago</source>
+        <translation>
+            <numerusform>шамамен %n күн бұрын</numerusform>
+            <numerusform>шамамен %n күн бұрын</numerusform>
+            <numerusform>шамамен %n күн бұрын</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../src/TwitterWidget.cpp" line="266"/>
+        <source>Just now</source>
+        <translation>Жаңа ғана</translation>
+    </message>
+</context>
+<context>
+    <name>UserpicsDownloader</name>
+    <message>
+        <location filename="../src/UserpicsDownloader.cpp" line="43"/>
+        <source>Downloading: %1</source>
+        <translation>Жүктеу: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/UserpicsDownloader.cpp" line="57"/>
+        <source>Error while downloading userpic: %1</source>
+        <translation>Пайдаланушының бейнесін жүктеу қатесі: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/UserpicsDownloader.cpp" line="62"/>
+        <source>Downloaded: %1</source>
+        <translation>Жүктелді: %1</translation>
+    </message>
+</context>
+</TS>
