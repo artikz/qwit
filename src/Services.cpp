a03858c290752f8661d89370c180e7204b24f120
artikz <artem.iglikov@gmail.com>
2010-08-19 03:58:02 +0000

    feature: integrated 3cm.kz url shortener
    git-svn-id: http://qwit.googlecode.com/svn/trunk@353 067a171b-6953-0410-b330-3d32600bf046

--- src/Services.cpp
+++ src/Services.cpp
@@ -3,28 +3,28 @@
  *
  *  @file
  *  @author Artem Iglikov <artem.iglikov@gmail.com>
- *  
+ *
  *  @section LICENSE
- *  
+ *
  *  This file is part of Qwit.
- *  
+ *
  *  Copyright (C) 2008, 2009 Artem Iglikov
- *  
+ *
  *  Qwit is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  Qwit is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with Qwit.  If not, see <http://www.gnu.org/licenses/>.
- *  
+ *
  *  @section DESCRIPTION
- *  
+ *
  *  Services class implementation
  */
 
@@ -263,6 +263,17 @@ void Services::initialize() {
 	migremeOptions["useusername"] = "false";
 	migremeOptions["defaultusername"] = "";
 	urlShorteners["migreme"] = migremeOptions;
+
+    QMap<QString, QString> _3cmkzOptions;
+    _3cmkzOptions["title"] = "3cm.kz";
+    _3cmkzOptions["apiurl"] = "http://3cm.kz/api/query/z6AmyKz2/";
+    _3cmkzOptions["requesttemplate"] = "?format=json&url=%url";
+    _3cmkzOptions["responseregexp"] = "http://3cm.kz/\\w+";
+    _3cmkzOptions["useapikey"] = "false";
+    _3cmkzOptions["defaultapikey"] = "";
+    _3cmkzOptions["useusername"] = "false";
+    _3cmkzOptions["defaultusername"] = "";
+    urlShorteners["3cmkz"] = _3cmkzOptions;
 }
 
 #endif
